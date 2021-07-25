38017947287dee3e67f8f7ab6e7711534e919211
Isaiah <idkburkes@gmail.com>
2021-07-25 17:35:20 -0500

    Completed KthLargestElementInArray

--- Medium/QuickSort/KthLargestElementInAnArray.java
+++ Medium/QuickSort/KthLargestElementInAnArray.java
@@ -2,28 +2,63 @@
 
 public class KthLargestElementInAnArray {
 
-  //Using Quicksort implementation
-    public int findKthLargest(int[] nums, int k) {
-        
+
+    public static void main(String[] args) {
+        int[] input = {3,2,1,5,6,4};
+        int k = 2;
+        System.out.print(findKthLargest(input, k));
     }
 
-    public static int partition(int[] nums, int k) {
-        //Use mid-of-three method to choose first partition value
-        int first = nums[0];
-        int mid = nums[nums.length / 2];
-        int last = nums[nums.length - 1];
-        int median = 0;
-        if(first <= mid && mid <= last) {
-            median = mid;
-        } else if(mid <= first && first <= last) {
-            median = first;
-        } else {
-            median = last;
+  //Using Quicksort implementation
+    public static int findKthLargest(int[] nums, int k) {
+        //Use normal partition process then use length - k to find largest  
+        return QuickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+
+    /**
+     * Method to swap two integers in an array
+     * @param nums
+     * @param a
+     * @param b
+     */
+    public static void swap(int[] nums,int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public static int QuickSelect(int[] nums, int start, int end, int k) {
+        if(nums.length == 0 || start > end) {
+            return -1;
         }
 
+        //Use last index for pivot
+        //Note (Can use random index or median-of-three for faster average time)
+        int pivotIndex = end;
+    
+        //Place partition value at end of array
+        int pivot = nums[pivotIndex];
+        swap(nums, pivotIndex, end);
+        int left = start;
+        for(int i = start; i < end; i++) {
+            //All values less than pivot go to left
+            if(nums[i] <= pivot) {
+                swap(nums, left++, i);  
+            }
+        }
+        //Swap pivot into correct position
+        swap(nums, left, end);
 
+        //Found correct value or use recursion on either side 
+        if(left == k) {
+            return nums[left];
+        } else if(left < k) { //Partition is too big, search right side
+            return QuickSelect(nums, left + 1, end, k); 
+        } else { //Partition value is too small, search left side
+            return QuickSelect(nums, start, left - 1, k);
 
-
+        }
     }
 
 
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Record of Leetcode problems that I've solved for quick reference to solution pat
 Problem | # | Difficulty | Algorithm/Strategy Applied | Solution Status
 --- | --- | --- | --- | --- |
 Find First and Last Position of Element in Sorted Array | 34 | Medium | Binary Search |  [Completed](https://github.com/idkburkes/Study-Of-Algorithms/blob/master/Medium/Binary%20Search/FindFirstAndLastPositionOfElementInSortedArray.java) |
-Kth Largest Element In An Array| 215 | Medium | QuickSort | [In Progress]()  |   
+Kth Largest Element In An Array| 215 | Medium | QuickSort | [In Progress](https://github.com/idkburkes/Study-Of-Algorithms/blob/master/Medium/QuickSort/KthLargestElementInAnArray.java)  |   
 |  | |  |   |   |
 |  | |  |   |   |
 |  | |  |   |   |
